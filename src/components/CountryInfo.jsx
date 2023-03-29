23a5651dd4531f1142e06347750de9f0a6e9a03e
alfarrobeira <122460653+alfarrobeira@users.noreply.github.com>
2023-03-29 19:07:11 +0200

    use destructuring in CountryInfo

--- src/components/CountryInfo.jsx
+++ src/components/CountryInfo.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import utils from "../services/utils.jsx";
 
 export default ({ countryCode }) => {
-    const [countryInfo, setCountryInfo] = useState();
+    // destructure object to avoid writing countryInfo over and over again, like countryInfo.flags, countryInfo.name, etc.
+    const [ { flags, name, subregion, region, area, population, capital, timezones, languages, currencies }, setCountryInfo] = useState({});
     const [loading, setLoading] = useState(true);
     const { getCountryInfo } = utils();
 
@@ -10,8 +11,6 @@ export default ({ countryCode }) => {
         setLoading(true);
 
         getCountryInfo(countryCode).then(data => {
-            console.log("CountryInfo:");
-            console.log(data);
             setCountryInfo(data);
             setLoading(false);
         });
@@ -25,31 +24,29 @@ export default ({ countryCode }) => {
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-3">
-                            <img src={countryInfo.flags.png} alt="Flag" />
+                            <img src={flags.png} alt="Flag" />
                         </div>
                         <div className="col-sm">
-                            <h4>{countryInfo.name.common}</h4>
-                            <div>{countryInfo.name.common} belongs to the region of <strong>{countryInfo.subregion ? countryInfo.subregion : countryInfo.region}</strong>.</div>
-                            <div>It covers an area of <strong>{countryInfo.area} m³</strong> where approx. <strong>{countryInfo.population} people</strong> live.</div>
-                            <div><em>Capital: </em>{countryInfo.capital}</div>
-                            <div><em>Timezone: </em>{countryInfo.timezones.length > 1 ? "s" : ""}:
-                                {countryInfo.timezones.length > 1 ? <ul>
-                                    {/* why do I have to use map? and not forEach? */}
-                                    {countryInfo.timezones.map(timezone =>
+                            <h4>{name.common}</h4>
+                            <div>{name.common} belongs to the region of <strong>{subregion ? subregion : region}</strong>.</div>
+                            <div>It covers an area of <strong>{area} m³</strong> where approx. <strong>{population} people</strong> live.</div>
+                            <div><em>Capital: </em>{capital}</div>
+                            <div><em>Timezone</em>{timezones.length > 1 ? "s" : ""}:
+                                {timezones.length > 1 ? <ul>
+                                    {/* Usage of array.map is required, because forEach does not expose the content of the loop */}
+                                    {timezones.map(timezone =>
                                         <li>{timezone}</li>
                                     )}
-                                </ul> : <span>&nbsp;{countryInfo.timezones[0]}</span>}
+                                </ul> : <span>&nbsp;{timezones[0]}</span>}
                             </div>
                             <div><em>People speak: </em>
                                 {/* convert languages object to array with Object.values()
                                 and back to string of concatenated elements with join() */}
-                                {Object.values(countryInfo.languages).join(", ")}
+                                {Object.values(languages).join(", ")}
                             </div>
                             <div><em>You can pay with: </em>
-                                {/* how to access name property of currency object? */}
-                                {console.log("Currencies 1: " + Object.values(countryInfo.currencies)['name'])}
-                                {console.log("Currencies 2: " + (Object.keys(countryInfo.currencies).values))}
-                                {Object.keys(countryInfo.currencies).join(", ")}
+                                {/* Nested objects! Access value of inner currency object */}
+                                { (Object.values(currencies).map(currency => <span>{currency.name}, </span>)) }
                             </div>
                         </div>
                     </div>
